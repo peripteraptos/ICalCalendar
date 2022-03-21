b7b4b7e4a325d25b0c416bc9181db9357d3927e2
Benedikt Ackermann <benacke@gmail.com>
2022-03-21 21:39:26 +0100

    calendar

--- applet/vite.config.js
+++ applet/vite.config.js
@@ -12,17 +12,5 @@ export default defineConfig({
       name: "CalendarApplet",
       fileName: format => `calendar.${format}.js`
     }
-    /*rollupOptions: {
-       // make sure to externalize deps that shouldn't be bundled
-       // into your library
-       external: ['vue'],
-       output: {
-         // Provide global variables to use in the UMD build
-         // for externalized deps
-         globals: {
-           vue: 'Vue'
-         }
-       }
-     }*/
   }
 });
