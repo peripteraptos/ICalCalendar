9767164cef2603640462cbc1bd871103776d20db
Benedikt Ackermann <benacke@gmail.com>
2022-03-23 23:12:35 +0100

    linkify + sanitize

--- applet/src/main.js
+++ applet/src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { vLinkify } from "v-linkify/src/index";
-
 const app = createApp(App);
-
-app.directive("linkify", vLinkify);
 app.mount("#vue-root");
