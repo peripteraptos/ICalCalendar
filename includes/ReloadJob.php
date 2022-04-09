3a45825953d3544bd7c4d0343ba8fdf2cb952b3f
Benedikt Ackermann <benacke@gmail.com>
2022-04-10 00:19:57 +0200

    reload job

---
+++ includes/ReloadJob.php
@@ -0,0 +1,20 @@
+<?php
+namespace MediaWiki\Extension\ICalCalendar;
+
+/**
+ * A job that reparses a wiki page, if time is come.
+ *
+ * @author Alexander Mashin
+ *
+ */
+class ReloadJob extends Job {
+	/**
+	 * @see Job::run
+	 *
+	 * @return bool True on success, false if something is very wrong.
+	 */
+	public function run() {
+		$store = new CalendarStore();
+		$store->fetch();
+	}
+}
