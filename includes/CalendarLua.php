bdddb5b5a0c34b097b6b1bd16a65a289f9608cee
Benedikt Ackermann <benacke@gmail.com>
2023-04-02 20:24:13 +0200

    added lua (?)

---
+++ includes/CalendarLua.php
@@ -0,0 +1,30 @@
+<?php
+namespace MediaWiki\Extension\ICalCalendar;
+
+/**
+ * Class for exposing the parser functions for Cargo to Lua.
+ * The functions are available via mw.ext.cargo Lua table.
+ *
+ * @author Yaron Koren.
+ * @author Alexander Mashin.
+ */
+class CalendarLua extends Scribunto_LuaLibraryBase {
+
+	/**
+	 * Register two Lua bindings: mw.ext.cargo.query and mw.ext.cargo.format
+	 * @return array|null
+	 */
+	public function register() {
+		$lib = [
+			'query' => [ $this, 'calendarQuery' ],
+		];
+		return $this->getEngine()->registerInterface( __DIR__ . '/../cargo.lua', $lib, [] );
+	}
+
+    public function calendarQuery(){
+
+        return [
+            ["name" => "test123", "id" => 123]
+        ];
+    }
+}
