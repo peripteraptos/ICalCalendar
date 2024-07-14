4d70d627b0cc2914f3cb76b657f34024d9f73a9d
Benedikt Ackermann <benacke@gmail.com>
2024-07-14 18:39:09 +0200

    add validUntil method and changed expire to 60s

--- includes/CalendarStore.php
+++ includes/CalendarStore.php
@@ -1,51 +1,70 @@
 <?php
 namespace MediaWiki\Extension\ICalCalendar;
 
-class CalendarStore {
+class CalendarStore
+{
 
-    public const CACHE_SECONDS = 3600;
+    public const CACHE_SECONDS = 60;
     public array $events = [];
     public bool $loaded = false;
 
-    public function getEvents(){
-        if(!$this->loaded){
-            if($this->cacheExists()){
+    public function getEvents()
+    {
+        if (!$this->loaded) {
+            if ($this->cacheExists()) {
                 $this->loadCache();
-            }else{
+            } else {
                 $this->fetch();
             }
         }
         return $this->events;
     }
 
-    public function getCacheFilePath(){
-        return wfTempDir()."/calendar.json";
+    public function getCategories()
+    {
+        global $wgCalendarSources;
+        return array_map(fn($item) => $item['color'], $wgCalendarSources);
     }
 
-    public function fetch(){
+    public function getCacheFilePath()
+    {
+        return wfTempDir() . "/calendar.json";
+    }
+
+    public function fetch()
+    {
         global $wgCalendarSources;
         $this->events = [];
-        foreach($wgCalendarSources as $name => $calendar){
-            $cal = new Calendar($calendar["url"],$name);
+        foreach ($wgCalendarSources as $name => $calendar) {
+            $cal = new Calendar($calendar["url"], $name);
             array_push($this->events, ...$cal->getMappedEvents());
         }
 
         $this->saveCache();
     }
 
-    public function saveCache(){
+    public function saveCache()
+    {
         return file_put_contents($this->getCacheFilePath(), json_encode($this->events));
     }
 
-    public function loadCache(){
-        return $this->events = json_decode(file_get_contents($this->getCacheFilePath()),true);
+    public function loadCache()
+    {
+        return $this->events = json_decode(file_get_contents($this->getCacheFilePath()), true);
     }
 
-    public function cacheOutdated(){
-        return time()-filemtime($this->getCacheFilePath()) > self::CACHE_SECONDS;
+    public function cacheOutdated()
+    {
+        return time() > $this->cacheValidUntil();
     }
 
-    public function cacheExists(){
+    public function cacheValidUntil()
+    {
+        return filemtime($this->getCacheFilePath()) + self::CACHE_SECONDS;
+    }
+
+    public function cacheExists()
+    {
         return file_exists($this->getCacheFilePath());
     }
 
